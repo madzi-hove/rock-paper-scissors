1dd0098bed7464e8e15f07a45c5b40fdc540c327
Madzi hove <madzihove@gmail.com>
2022-11-15 14:44:55 +0200

    Add new function game()

--- js/script.js
+++ js/script.js
@@ -5,24 +5,53 @@ const getComputerChoice = () => {
 	return choices[randomNumber];
 };
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt(
-	"Rock, Paper or Scissors?: "
-).toLocaleLowerCase();
-
 const playRound = (playerSelection, computerSelection) => {
-	if (playerSelection == computerSelection) return "No winner's! It's a draw";
+	if (playerSelection == computerSelection) return "draw";
 
 	if (playerSelection == "rock" && computerSelection == "paper")
-		return `You Lose! ${computerSelection} beats ${playerSelection}`;
+		return "computer";
 
 	if (playerSelection == "paper" && computerSelection == "scissors")
-		return `You Lose! ${computerSelection} beats ${playerSelection}`;
+		return "computer";
 
 	if (playerSelection == "scissors" && computerSelection == "rock")
-		return `You Lose! ${computerSelection} beats ${playerSelection}`;
+		return "computer";
 
-	return `You Win! ${playerSelection} beats ${computerSelection}`;
+	return "player";
 };
 
-console.log(playRound(playerSelection, computerSelection));
+const displayRoundWinner = (winner, playerSelection, computerSelection) => {
+	if (winner === "player") {
+		console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+	} else if (winner === "computer") {
+		console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+	} else console.log("Nobody wins! It's a draw");
+};
+
+const displayGameWinner = (playerCount, computerCount) => {
+	if (playerCount > computerCount) console.log("Player Wins!");
+	else console.log("Computer Wins!");
+};
+
+const game = () => {
+	let playerCount = 0;
+	let computerCount = 0;
+	for (let i = 0; i < 5; i++) {
+		const computerSelection = getComputerChoice();
+		const playerSelection = prompt(
+			"Rock, Paper or Scissors?: "
+		).toLocaleLowerCase();
+
+		const result = playRound(playerSelection, computerSelection);
+		if (result === "player") {
+			displayRoundWinner(result, playerSelection, computerSelection);
+			playerCount++;
+		} else if (result === "computer") {
+			displayRoundWinner(result, playerSelection, computerSelection);
+			computerCount++;
+		} else displayRoundWinner(result, playerSelection, computerSelection);
+	}
+	displayGameWinner(playerCount, computerCount);
+};
+
+game();
