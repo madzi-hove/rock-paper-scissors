935e6a8c0722ffdbfa6b7a2c4ab4ade7659dec7d
Madzi hove <madzihove@gmail.com>
2022-11-22 17:37:31 +0200

    Add user interface

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+// const container = document.querySelector(".container");
+// const scoreContainer = document.querySelector(".scores-container");
+const btnContainer = document.querySelector(".btn-container");
+const playerGameScore = document.querySelector(".player-score");
+const computerGameScore = document.querySelector(".computer-score");
+const message = document.querySelector(".message");
 const choices = ["rock", "paper", "scissors"];
+let playerCount = 0;
+let computerCount = 0;
 
+// let playerSelection;
 const getComputerChoice = () => {
 	const randomNumber = Math.floor(Math.random() * 3);
 	return choices[randomNumber];
@@ -21,37 +30,52 @@ const playRound = (playerSelection, computerSelection) => {
 };
 
 const displayRoundWinner = (winner, playerSelection, computerSelection) => {
+	let text;
 	if (winner === "player") {
-		console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+		text = `You Win! ${playerSelection} beats ${computerSelection}`;
 	} else if (winner === "computer") {
-		console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-	} else console.log("Nobody wins! It's a draw");
+		text = `You Lose! ${computerSelection} beats ${playerSelection}`;
+	} else text = "Nobody wins! It's a draw";
+	message.innerText = text;
 };
 
-const displayGameWinner = (playerCount, computerCount) => {
-	if (playerCount > computerCount) console.log("Player Wins!");
-	else console.log("Computer Wins!");
-};
-
-const game = () => {
-	let playerCount = 0;
-	let computerCount = 0;
-	for (let i = 0; i < 5; i++) {
-		const computerSelection = getComputerChoice();
-		const playerSelection = prompt(
-			"Rock, Paper or Scissors?: "
-		).toLocaleLowerCase();
-
-		const result = playRound(playerSelection, computerSelection);
-		if (result === "player") {
-			displayRoundWinner(result, playerSelection, computerSelection);
-			playerCount++;
-		} else if (result === "computer") {
-			displayRoundWinner(result, playerSelection, computerSelection);
-			computerCount++;
-		} else displayRoundWinner(result, playerSelection, computerSelection);
+const displayGameWinner = (playerScore, computerScore) => {
+	if (playerScore === 5 || computerScore === 5) {
+		if (playerScore > computerScore)
+			message.textContent = "You have won the best of 5!";
+		else message.textContent = "Computer has won the best of 5!";
+		playerGameScore.textContent = 0;
+		computerGameScore.textContent = 0;
+		playerCount = 0;
+		computerCount = 0;
+		setTimeout(() => {
+			message.textContent = "Best of 5, Let's Play";
+		}, 3000);
 	}
+};
+
+const game = (playerSelection) => {
+	const computerSelection = getComputerChoice();
+	const result = playRound(playerSelection, computerSelection);
+	if (result === "player") {
+		displayRoundWinner(result, playerSelection, computerSelection);
+		playerCount++;
+		playerGameScore.textContent = playerCount;
+	} else if (result === "computer") {
+		displayRoundWinner(result, playerSelection, computerSelection);
+		computerCount++;
+		computerGameScore.textContent = computerCount;
+	} else displayRoundWinner(result, playerSelection, computerSelection);
+
 	displayGameWinner(playerCount, computerCount);
 };
 
-game();
+const playGame = () => {
+	message.textContent = "Best of 5, Let's Play";
+	btnContainer.addEventListener("click", (e) => {
+		const playerChoice = e.target.id.toLowerCase();
+		game(playerChoice);
+	});
+};
+
+playGame();
